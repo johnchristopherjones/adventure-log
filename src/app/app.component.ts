faea7e7b291c503c51c7ad378ac2b1a7be3b5e1a
John Christopher Jones <john.christopher@alumni.virginia.edu>
2020-05-17 18:43:52 -0400

    chore: restart project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import { MdIconRegistry } from '@angular/material';
-
-import { Character, DataService } from './data.service';
-import { IconService } from './icon.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  private characters: Character[];
-  private books = {
-    ph: true,
-    sc: true,
-    vg: true
-  };
-
-  constructor(
-    private data: DataService,
-    private icon: IconService
-  ) {
-  }
-
-  ngOnInit() {
-    this.characters = [this.data.newCharacter()];
-  }
-
-  private changeCharacter(character: Character, index: number) {
-    this.characters[index] = Object.assign(this.characters[index], character);
-  }
-
-  private insertCharacter(index: number) {
-    this.characters = [
-      ...this.characters.slice(0, index + 1),
-      this.data.randomCharacter(),
-      ...this.characters.slice(index + 1, this.characters.length)
-    ];
-  }
+export class AppComponent {
+  title = 'character-sheet';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { MaterialModule } from '@angular/material';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DemoComponent } from './demo/demo.component';
-import { DataService } from './data.service';
-import { IconService } from './icon.service';
-import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DemoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule,
-    MaterialModule.forRoot(),
-
-    ComponentsModule,
+    AppRoutingModule
   ],
-  providers: [
-    DataService,
-    IconService,
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
